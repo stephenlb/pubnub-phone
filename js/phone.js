cee042ed0b375e48184443d795b144daad461071
Stephen L. Blum <blum.stephen@gmail.com>
2012-07-07 10:59:17 -0700

    fixed easy End Call UI bug.

--- js/phone.js
+++ js/phone.js
@@ -62,17 +62,19 @@ function show_call_status(args) {
 // HIDE VIA ANIMATION
 function hide_screen(screen) {
     animate( screen, [
-        { d : 0.4, s : 0.7, opacity : 0, 'z-index' : 100 }
+        { d : 0.4, s : 0.7, opacity : 0 }
     ], function() {
+        p.css( screen, { 'z-index' : 100 } );
     } );
 }
 
 // SHOW VIA ANIMATION
 function show_screen(screen) {
     animate( screen, [
-        { d : 0.0001, s : 0.5, opacity : 0.7, 'z-index' : 10000 },
+        { d : 0.0001, s : 0.5, opacity : 0.7 },
         { d : 0.4,    s : 1,   opacity : 1 }
     ], function() {
+        p.css( screen, { 'z-index' : 10000 } );
     } );
 }
 
